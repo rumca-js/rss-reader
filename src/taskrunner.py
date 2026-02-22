d3aa1810498dab9d55638ea1981df61d62dfcec7
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2026-02-22 12:29:47 +0100

    Fix

--- src/taskrunner.py
+++ src/taskrunner.py
@@ -1,7 +1,7 @@
 import re
 import time
 from datetime import datetime, timedelta
-from webtoolkit import BaseUrl, RemoteUrl
+from webtoolkit import BaseUrl, RemoteUrl, PageRequestObject
 import traceback
 
 from .dbconnection import DbConnection
